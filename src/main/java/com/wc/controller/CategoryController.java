4c999c42dca7ee10e0f2d47d4b4c94e32f3b57b3
wangcheng666 <2723349107@qq.com>
2024-03-12 09:44:25 +0800

    3月12日，文章分类更新接口参数校验冲突bug修复，添加分组校验

--- src/main/java/com/wc/controller/CategoryController.java
+++ src/main/java/com/wc/controller/CategoryController.java
@@ -16,7 +16,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
     @PostMapping
-    public Result add(@RequestBody @Validated Category category){
+    public Result add(@RequestBody @Validated(Category.Add.class) Category category){
         categoryService.add(category);
         return Result.success();
     }
@@ -31,7 +31,7 @@ public class CategoryController {
     }
 
     @PutMapping
-    public Result update(@RequestBody @Validated Category category){
+    public Result update(@RequestBody @Validated(Category.Update.class) Category category){
         categoryService.update(category);
         return Result.success();
     }
--- src/main/java/com/wc/pojo/Category.java
+++ src/main/java/com/wc/pojo/Category.java
@@ -3,6 +3,7 @@ package com.wc.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.NonNull;
 
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;//主键ID
     @NotEmpty
     private String categoryName;//分类名称
@@ -21,4 +22,12 @@ public class Category {
     private LocalDateTime createTime;//创建时间
     @JsonFormat(pattern = "yyyy-MM-dd HH-mm-ss")
     private LocalDateTime updateTime;//更新时间
+
+    public interface Add extends Default {
+
+    }
+
+    public interface Update extends Default{
+
+    }
 }
